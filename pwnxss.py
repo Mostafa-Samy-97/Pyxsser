8bbb441c23f474354a6a7533623ed8cabf83b000
Mostafa-Samy-CEH <51253114+Mostafa-Samy-97@users.noreply.github.com>
2022-01-26 20:08:43 -0800

    Update pwnxss.py

--- pwnxss.py
+++ pwnxss.py
@@ -1,8 +1,3 @@
-'''
-PwnXSS - 2019/2020
-This project was created by Andripwn with Pwn0sec team.
-Copyright under the MIT license
-'''
 import argparse
 from lib.helper.helper import *
 from lib.helper.Log import *
@@ -10,8 +5,8 @@ from lib.core import *
 from random import randint
 from lib.crawler.crawler import *
 epilog="""
-Github: https://www.github.com/pwn0sec/PwnXSS
-Version: 0.5 Final
+Github: https://github.com/Mostafa-Samy-97/Pyxsser
+Version: 1.0
 """
 def check(getopt):
 	payload=int(getopt.payload_level)
@@ -34,7 +29,7 @@ def start():
 	
 	pos_opt=parse.add_argument_group("Options")
 	pos_opt.add_argument("--help",action="store_true",default=False,help="Show usage and help parameters")
-	pos_opt.add_argument("-u",metavar="",help="Target url (e.g. http://testphp.vulnweb.com)")
+	pos_opt.add_argument("-u",metavar="",help="Target url (e.g. http://example.com)")
 	pos_opt.add_argument("--depth",metavar="",help="Depth web page to crawl. Default: 2",default=2)
 	pos_opt.add_argument("--payload-level",metavar="",help="Level for payload Generator, 7 for custom payload. {1...6}. Default: 6",default=6)
 	pos_opt.add_argument("--payload",metavar="",help="Load custom payload directly (e.g. <script>alert(2005)</script>)",default=None)
@@ -42,12 +37,12 @@ def start():
 	pos_opt.add_argument("--user-agent",metavar="",help="Request user agent (e.g. Chrome/2.1.1/...)",default=agent)
 	pos_opt.add_argument("--single",metavar="",help="Single scan. No crawling just one address")
 	pos_opt.add_argument("--proxy",default=None,metavar="",help="Set proxy (e.g. {'https':'https://10.10.1.10:1080'})")
-	pos_opt.add_argument("--about",action="store_true",help="Print information about PwnXSS tool")
+	pos_opt.add_argument("--about",action="store_true",help="Print information about Pyxsser tool")
 	pos_opt.add_argument("--cookie",help="Set cookie (e.g {'ID':'1094200543'})",default='''{"ID":"1094200543"}''',metavar="")
 	
 	getopt=parse.parse_args()
 	print(logo)
-	Log.info("Starting PwnXSS...")
+	Log.info("Starting Pyxsser...")
 	if getopt.u:
 		core.main(getopt.u,getopt.proxy,getopt.user_agent,check(getopt),getopt.cookie,getopt.method)
 		
@@ -59,10 +54,7 @@ def start():
 	elif getopt.about:
 		print("""
 ***************
-Project: PwnXSS
-License: MIT
 Author: Security Executions Code
-Last updates: 2019 may 26
 Note: Take your own RISK
 ****************
 """+epilog)
